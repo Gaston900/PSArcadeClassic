a19a7d88f2d17b49a6acdab4bd143f946623bbb2
LAPTOP-E6VUUOVQ\Gasto <unknownplayers940@gmail.com>
2026-09-08 15:48:59 -0300

    DPI "UI" Fix

--- src/osd/winui/winui.cpp
+++ src/osd/winui/winui.cpp
@@ -2925,7 +2925,7 @@ static void InitListTree(void)
 		TreeView_SetBkColor(hMainTreeView, GetFolderBgColor());
 		TreeView_SetTextColor(hMainTreeView, GetTreeFontColor()); 
 
-		DWORD dwExStyle = TVS_EX_AUTOHIDEBUTTONS | TVS_EX_FADEINOUTEXPANDOS;
+		DWORD dwExStyle = 0;
 		SendMessage(hMainTreeView, TVM_SETEXTENDEDSTYLE, dwExStyle, dwExStyle);
 
 		InvalidateRect(hMainTreeView, NULL, TRUE);
